416a594b396af1909b91df939b20c878a148e247
Summy Singh <summysingh86@gmail.com>
2024-06-10 00:57:01 +0530

    Destructor & Dispose Programs

---
+++ CSharp_Learning/80_Destructor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Destructor Example
+namespace _80_Destructor
+{
+    class Sample
+    {
+        //constructor
+        public Sample()
+        {
+            Console.WriteLine("Constructor of sample class");
+        }
+
+        //destructor
+        ~Sample()
+        {
+            Console.WriteLine("Destructor is called");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp_Learning/81_Dispose/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Dispose Example
+namespace _81_Dispose
+{
+    class Sample : System.IDisposable
+    {
+        //constructor
+        public Sample()
+        {
+            Console.WriteLine("Constructor is called");
+        }
+        //method
+        public void DisplayData()
+        {
+            Console.WriteLine("Some method is doing operation");
+        }
+
+        //Dispose Method
+        public void Dispose()
+        {
+            Console.WriteLine("Dipose method will work here");
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object using "using structure"
+            using (Sample s = new Sample())
+            {
+                s.DisplayData();
+            }
+            Console.ReadLine();
+        }
+    }
+}
