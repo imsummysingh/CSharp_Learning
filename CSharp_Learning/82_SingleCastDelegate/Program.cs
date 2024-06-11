473b92181bfdd5cc8d75fd438df979ab9379fbc9
Summy Singh <summysingh86@gmail.com>
2024-06-11 21:28:40 +0530

    Delegate Progs

---
+++ CSharp_Learning/82_SingleCastDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Single cast delegate example
+namespace _82_SingleCastDelegate
+{
+    //Sample class
+    class Sample
+    {
+        //target method
+        public int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+
+    //delgate
+    public delegate int MyDelegateType(int a,int b);
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of sample class
+            Sample s = new Sample();
+
+            //create deleagte or delegate object
+            MyDelegateType del;
+
+            //add method reference to delegate
+            del = new MyDelegateType(s.Add);
+
+            //invoking add method using delegate object
+            //del.Invoke(2,3);
+            Console.WriteLine(del.Invoke(2, 3));
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSharp_Learning/83_MultiCastDelegate/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//multi cast delegate example
+namespace _83_MultiCastDelegate
+{
+    class Sample
+    {
+        //target method 1
+        public void Add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Addition of two numbers is:" + c);
+        }
+
+        //target file 2
+        public void Multiply(int a, int b)
+        {
+            int c = a * b;
+            Console.WriteLine("Mulitplication of two numbers is:" + c);
+        }        
+    }
+
+    //delegate
+    public delegate void MyDelegateType(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+            //MyDelegateType md = new MyDelegateType(s.Add);
+            MyDelegateType md;
+            
+            //add ref of first target method
+            md = s.Add;
+
+            //add ref of second method. -> use '+' after delagte reference varibale, to chain it.
+            md += s.Multiply;
+
+            //invoke both target method -> 1.Add, 2.Muliply
+            md.Invoke(10, 20);
+
+
+            Console.ReadLine();
+        }
+    }
+}
