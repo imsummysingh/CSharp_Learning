8d8894546183f6f4c87c0606193c594c0c448844
Summy Singh <summysingh86@gmail.com>
2024-06-19 00:53:29 +0530

    Stack & Queue Programs

---
+++ CSharp_Learning/H_139_Stack/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Stack Example
+namespace H_139_Stack
+{
+    class Student
+    {
+        public int Marks { get; set; }
+        public int Rank { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of stack
+            Stack<int> marks = new Stack<int>();
+
+            //add
+            marks.Push(45);
+            marks.Push(61);
+            marks.Push(80);
+            marks.Push(93);
+
+            //foreach for accesing the stack
+            Console.WriteLine("Pushing values in stack");
+            foreach (int item in marks)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //remove
+            marks.Pop();
+            marks.Pop();
+            Console.WriteLine("After removing last 2 elements");
+            foreach (int item in marks)
+            {
+                Console.WriteLine(item);
+            }
+
+            //peek
+            Console.WriteLine("Peek: " + marks.Peek());
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Create student type object of stack
+            Stack<Student> stu = new Stack<Student>();
+
+            //add
+            stu.Push(new Student() { Marks = 45 });
+            stu.Push(new Student() { Marks = 81 });
+            stu.Push(new Student() { Marks = 89 });
+
+            Console.WriteLine("Student Type Object with Stack");
+
+            //accesing the value
+            int r = 1;
+            foreach(Student s in stu)
+            {
+                s.Rank = r;
+                Console.WriteLine(s.Marks +" , "+s.Rank);
+                r++;
+            }
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp_Learning/H_140_QueueExample/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Queue Example
+namespace H_140_QueueExample
+{
+    class Student
+    {
+        public int Marks { get; set; }
+        public int Rank { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of stack
+            Queue<int> marks = new Queue<int>();
+
+            //add
+            marks.Enqueue(45);
+            marks.Enqueue(61);
+            marks.Enqueue(80);
+            marks.Enqueue(93);
+
+            //foreach for accesing the stack
+            Console.WriteLine("Enqueue(Insert) values in Queue");
+            foreach (int item in marks)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //remove
+            marks.Dequeue();
+            marks.Dequeue();
+            Console.WriteLine("After removing first 2 elements");
+            foreach (int item in marks)
+            {
+                Console.WriteLine(item);
+            }
+
+            //peek
+            Console.WriteLine("Peek: " + marks.Peek());
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Create student type object of stack
+            Queue<Student> stu = new Queue<Student>();
+
+            //add
+            stu.Enqueue(new Student() { Marks = 45 });
+            stu.Enqueue(new Student() { Marks = 81 });
+            stu.Enqueue(new Student() { Marks = 89 });
+
+            Console.WriteLine("Student Type Object with Stack");
+
+            //accesing the value
+            int r = 1;
+            foreach (Student s in stu)
+            {
+                s.Rank = r;
+                Console.WriteLine(s.Marks + " , " + s.Rank);
+                r++;
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
