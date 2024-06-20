09e625cb3bcdab74653b2c63fe05476c08b44e1b
Summy Singh <summysingh86@gmail.com>
2024-06-21 00:20:39 +0530

    Linq Programs added

---
+++ CSharp_Learning/H_160_Linq/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Linq example
+namespace H_160_Linq
+{
+    class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Job { get; set; }
+        public string City { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //collection of objects
+            List<Employee> emp = new List<Employee>()
+            {
+                new Employee(){EmpId=101, EmpName="Ranjeet", Job="Developer", City="Bangalore"},
+                new Employee(){EmpId=102, EmpName="Vishal", Job="Testing", City="Bangalore"},
+                new Employee(){EmpId=103, EmpName="Mani", Job="Cloud",City="Gurgaon"},
+                new Employee(){EmpId=104, EmpName="Vicky",Job="DevOps", City="Leeds"}
+            };
+
+            //linq query
+
+            Console.WriteLine("Linq query to find Job profile as Developer");
+            var result = emp.Where(e=>e.Job=="Developer");
+            foreach(var item in result)
+            {
+                Console.WriteLine(item.EmpId+", "+item.EmpName+", "+item.Job+", "+item.City);
+            }
+
+
+            Console.WriteLine("\nLinq query to find emp living in Bangalore");
+            //var empCity = emp.Where(e => e.City == "Bangalore");
+            //IEnumerable<Employee> empCity = emp.Where(e => e.City == "Noida"); -> will give null
+            IEnumerable<Employee> empCity = emp.Where(e => e.City == "Bangalore");
+            foreach (var c in empCity)
+            {
+                Console.WriteLine(c.EmpId + ", " + c.EmpName + ", " + c.Job + ", " + c.City);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp_Learning/H_161_LinqOrderBy/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//linq order by example
+namespace H_161_LinqOrderBy
+{
+    class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Job { get; set; }
+        public double Salary { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //collection of objects
+            List<Employee> emp = new List<Employee>()
+            {
+                new Employee(){EmpId=101, EmpName="Ranjeet", Job="Developer", Salary=90000},
+                new Employee(){EmpId=102, EmpName="Vishal", Job="Testing", Salary=110000},
+                new Employee(){EmpId=103, EmpName="Mani", Job="Cloud",Salary=78000},
+                new Employee(){EmpId=104, EmpName="Vicky",Job="DevOps", Salary=120000},
+                new Employee(){EmpId=105, EmpName="Govind", Job="Cloud", Salary=6000},
+            };
+
+            //orderby ->Ascending order
+            //return data in IOrderEnumerable
+            //IEnumerable<Employee> sortedEmp = emp.OrderBy(e => e.EmpName);
+            IOrderedEnumerable<Employee> sortedEmp=emp.OrderBy(e => e.EmpName);
+            Console.WriteLine("OrderBy Name:");
+            foreach(Employee e in sortedEmp)
+            {
+                Console.WriteLine(e.EmpId + ", " + e.EmpName + ", " + e.Job + ", " + e.Salary);
+            }
+
+            IOrderedEnumerable<Employee> sortedEmpDesc = emp.OrderByDescending(e => e.EmpName);
+            Console.WriteLine("\nOrderBy Name Descending Order:");
+            foreach (Employee e in sortedEmpDesc)
+            {
+                Console.WriteLine(e.EmpId + ", " + e.EmpName + ", " + e.Job + ", " + e.Salary);
+            }
+
+
+            //ThenBy
+            IOrderedEnumerable<Employee> sortthenEmp = emp.OrderBy(e => e.Job).ThenBy(e=>e.Salary);
+            Console.WriteLine("\nOrderBy Job & ThenBySalary:");
+            foreach (Employee e in sortthenEmp)
+            {
+                Console.WriteLine(e.EmpId + ", " + e.EmpName + ", " + e.Job + ", " + e.Salary);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
