74abe5f6ba541c7d30d6d2cdd14501e7cb17f91d
Summy Singh <summysingh86@gmail.com>
2024-06-21 17:09:02 +0530

    String Prog and new solution added

---
+++ CSharp_Learning_Pt2/01_String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//String example
+namespace _01_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Hello";
+            String y = "Hey";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+
+            Console.WriteLine(x.Length);    //5
+            Console.WriteLine(y[1]);        //e
+
+            Console.ReadLine();
+        }
+    }
+}
